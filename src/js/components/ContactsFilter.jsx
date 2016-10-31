a402dadac676dbccf809de0e2ffa8f31ea8c9725
Guillaume Lambert <glambertmtl@gmail.com>
2016-10-30 20:42:57 -0400

    Set filter input to text instead of search

--- src/js/components/ContactsFilter.jsx
+++ src/js/components/ContactsFilter.jsx
@@ -3,7 +3,7 @@ import React, { PropTypes, Component } from 'react';
 const ContactsFilter = ({ onChange }) => {
   return (
     <form className="u-mb-2" onSubmit={(event) => { event.preventDefault(); }}>
-      <input className="form-control" type="search" placeholder="Filter Contacts"
+      <input className="form-control" type="text" placeholder="Filter Contacts"
         onChange={(event) => onChange(event.target.value)} />
     </form>
   )
